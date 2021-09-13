56432470683ad32559dfd5d46e895a18d0accff6
baja1116 <90457347+baja1116@users.noreply.github.com>
2021-09-13 08:27:59 -0400

    Update controllflow.py

--- controllflow.py
+++ controllflow.py
@@ -1,12 +1,12 @@
 grade = 86
 
-if grade >= 90:
+if grade >= 99:
   print("A")
-elif grade >= 80:
+elif grade >= 90:
   print("B")
-elif grade >= 70:
+elif grade >= 80:
   print("C")
-elif grade >= 60:
+elif grade >= 70:
   print("D")
 else:
   print("F")
